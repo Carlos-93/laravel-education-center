cae8e629e414ef2307c9663c21e49cd99bcbabdb
Carlos-93 <carlos93.bcn@hotmail.com>
2024-04-22 13:20:37 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Title -->
-    <title>Lareduca</title>
+    <title>Tech-Play Education</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
