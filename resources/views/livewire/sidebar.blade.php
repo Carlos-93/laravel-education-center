62d134681b877b0122e02c7be147108238d24363
Carlos-93 <carlos93.bcn@hotmail.com>
2024-04-29 20:28:16 +0200

    update

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -7,133 +7,70 @@
             <div class="flex items-center justify-center mb-10">
                 <img src="/images/logo.png" alt="Logo Tech-Play Education" class="w-52">
             </div>
-            <div class="flex flex-col gap-10 h-full w-full justify-between">
-                <ul class="flex flex-col gap-10">
-                    <a href="{{ route('dashboard') }}"
-                        class="{{ request()->routeIs('dashboard') ? 'text-yellow-400 translate-x-3' : 'text-white hover:text-yellow-400' }} flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200">
-                        <li>
-                            <span class="inline-flex items-center justify-center">
+            <div class="flex flex-col h-full w-full justify-between pl-16 font-bold">
+                <ul class="flex flex-col gap-10 justify-center items-start">
+                    <li>
+                        <a href="{{ route('dashboard') }}"
+                            class="{{ request()->routeIs('dashboard') ? 'text-yellow-400' : 'text-white hover:text-yellow-400' }} flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200">
+                            <span>
                                 <i class="bx bx-home text-2xl"></i>
                             </span>
-                            <span class="font-bold">Dashboard</span>
-                        </li>
-                    </a>
-
-                    <a href="{{ route('course-management') }}"
-                        class="{{ request()->routeIs('course-management') ? 'text-yellow-400 translate-x-3' : 'text-white hover:text-yellow-400' }} w-full  flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200">
-                        <li>
-                            <span class="inline-flex items-center justify-center">
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('course-management') }}"
+                            class="{{ request()->routeIs('course-management') ? 'text-yellow-400' : 'text-white hover:text-yellow-400' }} flex items-start gap-3 transform hover:translate-x-3 transition-transform ease-in duración-200">
+                            <span>
                                 <i class="bx bxs-school text-2xl"></i>
                             </span>
-                            <span class="font-bold">Courses</span>
-                        </li>
-                    </a>
-
-                    <a href="#"
-                        class="flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200 text-white hover:text-yellow-400">
-                        <span class="inline-flex items-center justify-center">
-                            <li>
-                                <i class="bx bxs-calendar text-2xl"></i>
-                        </span>
-                        <span class="font-bold">Calendar</span>
-                        </li>
-                    </a>
+                            <span>Courses</span>
+                        </a>
+                    </li>
                     <li>
                         <a href="#"
-                            class="flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200 text-white hover:text-yellow-400">
-                            <span class="inline-flex items-center justify-center">
+                            class="flex items-start gap-3 transform hover:translate-x-3 transition-transform ease-in duración-200 text-white hover:text-yellow-400">
+                            <span>
+                                <i class="bx bxs-calendar text-2xl"></i>
+                            </span>
+                            <span>Calendar</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-start gap-3 transform hover:translate-x-3 transition-transform ease-in duración-200 text-white hover:text-yellow-400">
+                            <span>
                                 <i class="bx bxs-conversation text-2xl"></i>
                             </span>
-                            <span class="font-bold">Staff Room</span>
+                            <span>Staff Room</span>
                         </a>
                     </li>
                 </ul>
-                <ul class="flex flex-col gap-10">
+
+                <ul class="flex flex-col gap-10 justify-center items-start">
                     <li>
                         <a href="{{ route('profile.show') }}"
-                            class="{{ request()->routeIs('profile.show') ? 'text-yellow-400 translate-x-3' : 'text-white hover:text-yellow-400' }} flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200">
-                            <span class="inline-flex items-center justify-center">
+                            class="{{ request()->routeIs('profile.show') ? 'text-yellow-400 translate-x-3' : 'text-white hover:text-yellow-400' }} flex items-start gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200">
+                            <span>
                                 <i class="bx bxs-cog text-2xl"></i>
                             </span>
-                            <span class="font-bold">Settings</span>
+                            <span>Settings</span>
                         </a>
                     </li>
-                    <form method="POST" action="{{ route('logout') }}" x-data>
-                        @csrf
-                        <li>
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}" x-data>
+                            @csrf
                             <a href="{{ route('logout') }}" @click.prevent="$root.submit();"
-                                class="flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200 text-yellow-400 hover:text-yellow-400">
-                                <span class="inline-flex items-center justify-center">
+                                class="flex items-start gap-3 transform hover:translate-x-3 transition-transform ease-in duración-200 text-yellow-400 hover:text-yellow-400">
+                                <span>
                                     <i class="bx bx-log-out text-2xl"></i>
                                 </span>
-                                <span class="font-bold">Log Out</span>
+                                <span>Log Out</span>
                             </a>
-                        </li>
-                    </form>
+                        </form>
+                    </li>
                 </ul>
             </div>
-            {{-- <ul class="flex flex-col pr-10 gap-10">
-                <li>
-                    <a href="{{ route('dashboard') }}"
-                        class="{{ request()->routeIs('dashboard') ? 'text-yellow-400 translate-x-3' : 'text-white hover:text-yellow-400' }} flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200">
-                        <span class="inline-flex items-center justify-center">
-                            <i class="bx bx-home text-2xl"></i>
-                        </span>
-                        <span class="font-bold">Dashboard</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('course-management') }}"
-                        class="{{ request()->routeIs('course-management') ? 'text-yellow-400 translate-x-3' : 'text-white hover:text-yellow-400' }} flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200">
-                        <span class="inline-flex items-center justify-center">
-                            <i class="bx bxs-school text-2xl"></i>
-                        </span>
-                        <span class="font-bold">Courses</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#"
-                        class="flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200 text-white hover:text-yellow-400">
-                        <span class="inline-flex items-center justify-center">
-                            <i class="bx bxs-calendar text-2xl"></i>
-                        </span>
-                        <span class="font-bold">Calendar</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200 text-white hover:text-yellow-400">
-                        <span class="inline-flex items-center justify-center">
-                            <i class="bx bxs-conversation text-2xl"></i>
-                        </span>
-                        <span class="font-bold">Staff Room</span>
-                    </a>
-                </li>
-                
-                <li>
-                    <a href="{{ route('profile.show') }}"
-                        class="{{ request()->routeIs('profile.show') ? 'text-yellow-400 translate-x-3' : 'text-white hover:text-yellow-400' }} flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200">
-                        <span class="inline-flex items-center justify-center">
-                            <i class="bx bxs-cog text-2xl"></i>
-                        </span>
-                        <span class="font-bold">Settings</span>
-                    </a>
-                </li>
-                <form method="POST" action="{{ route('logout') }}" x-data>
-                    @csrf
-                    <li>
-                        <a href="{{ route('logout') }}" @click.prevent="$root.submit();"
-                            class="flex items-center gap-3 transform hover:translate-x-3 transition-transform ease-in duration-200 text-yellow-400 hover:text-yellow-400">
-                            <span class="inline-flex items-center justify-center">
-                                <i class="bx bx-log-out text-2xl"></i>
-                            </span>
-                            <span class="font-bold">Log Out</span>
-                        </a>
-                    </li>
-                </form>
-            </ul> --}}
         </div>
     </aside>
 </div>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -21,7 +21,6 @@
                                     <button type="button"
                                         class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                         {{ Auth::user()->currentTeam->name }}
-
                                         <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                                             fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -78,16 +77,19 @@
                                         src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                 </button>
                             @else
-                                <span class="inline-flex rounded-md">
+                                <span class="inline-flex flex-col rounded-md">
                                     <button type="button"
-                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                        {{ Auth::user()->name }}
-
-                                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                        </svg>
+                                        class="inline-flex flex-col text-start px-3 py-2 border border-transparent leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                        <p class="mb-1 font-bold text-black">{{ Auth::user()->name }} ({{ ucfirst(Auth::user()->role) }})</p>
+                                        <div class="inline-flex items-center">
+                                            <p class="text-sm">{{ Auth::user()->email }}</p>
+                                            <svg class="ms-2 -me-0.5 h-4 w-4 text-black" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </div>
                                     </button>
                                 </span>
                             @endif
